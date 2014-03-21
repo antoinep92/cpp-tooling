16d7ad4dc55276885ad574e00a1e624f6c9c8c95
Antoine Poliakov <apoliakov@aneo.fr>
2014-03-21 15:47:53 +0100

    added 6 existing clang actions

--- main.cpp
+++ main.cpp
@@ -18,22 +18,50 @@
 
 #include "simple-parse.h"
 
-static llvm::cl::OptionCategory ActionSelectorOptions("Action selector options");
-static llvm::cl::extrahelp CommonHelp(clang::tooling::CommonOptionsParser::HelpMessage);
-static llvm::cl::extrahelp MoreHelp("\nThese options are for selecting the action on the code");
-static llvm::cl::opt<bool> ActionParse("action-parse", llvm::cl::desc("parse C++ code, print classes and functions and what they use"));
+static llvm::cl::OptionCategory option_category("Action selector options");
+static llvm::cl::extrahelp options_help(clang::tooling::CommonOptionsParser::HelpMessage);
+
+// developped here
+static llvm::cl::opt<bool>
+print_use_graph("print-use-graph",		llvm::cl::desc("parse + print classes/functions declarations and what they use"));
+
+// from clang
+static llvm::cl::opt<bool>
+print_lex(		"print-lex",			llvm::cl::desc("lex + print tokens before pre-processor"));
+static llvm::cl::opt<bool>
+print_preproc(	"print-preprocess",		llvm::cl::desc("lex + print tokens after pre-processor"));
+static llvm::cl::opt<bool>
+print_macros(	"print-macros",			llvm::cl::desc("print #defines"));
+static llvm::cl::opt<bool>
+print_decls(	"print-declarations",	llvm::cl::desc("parse + print declarations"));
+static llvm::cl::opt<bool>
+print_ast(		"print-ast",			llvm::cl::desc("parse + print the whole AST"));
+static llvm::cl::opt<bool>
+print_source(	"print-source",			llvm::cl::desc("parse + resynthetize source"));
 
 int main(int argc, const char **argv) {
-	clang::tooling::CommonOptionsParser OptionsParser(argc, argv);
-	clang::tooling::ClangTool Tool(OptionsParser.getCompilations(), OptionsParser.getSourcePathList());
+	clang::tooling::CommonOptionsParser option_parser(argc, argv);
+	clang::tooling::ClangTool tool(option_parser.getCompilations(), option_parser.getSourcePathList());
 
 	std::function<int()> action;
-	if(ActionParse) action = [&](){ return Tool.run(clang::tooling::newFrontendActionFactory<SimpleParseAction>()); };
+	if(print_use_graph)
+		action = [&](){ return tool.run(clang::tooling::newFrontendActionFactory<PrintUseGraph>()); };
+	if(print_lex)
+		action = [&](){ return tool.run(clang::tooling::newFrontendActionFactory<clang::DumpRawTokensAction>()); };
+	if(print_preproc)
+		action = [&](){ return tool.run(clang::tooling::newFrontendActionFactory<clang::DumpTokensAction>()); };
+	if(print_macors)
+		action = [&](){ return tool.run(clang::tooling::newFrontendActionFactory<clang::PrintPreprocessedAction>()); };
+	if(print_decls)
+		action = [&](){ return tool.run(clang::tooling::newFrontendActionFactory<clang::ASTDeclListAction>()); };
+	if(print_ast)
+		action = [&](){ return tool.run(clang::tooling::newFrontendActionFactory<clang::ASTDumpAction>()); };
+	if(print_source)
+		action = [&](){ return tool.run(clang::tooling::newFrontendActionFactory<clang::ASTPrintAction>()); };
 
 	if(!action) {
 		std::cerr << "no known action selected" << std::endl;
 		return 1;
-	}
-	return action();
+	} else return action();
 }
 
