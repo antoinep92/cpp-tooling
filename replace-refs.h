73ceba4b40f456f6376fc494d1765215f3228267
Antoine Poliakov <apoliakov@aneo.fr>
2014-05-07 22:17:57 +0200

    Implemented 2/3 phases of replace refs

--- replace-refs.h
+++ replace-refs.h
@@ -19,62 +19,130 @@
 
 #include <iostream>
 #include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+
+struct ReplaceRefs {
+
+	typedef std::unordered_map<
+		std::string,	/* qualified original name */
+		std::string		/* unqualified target name */
+	> Renames;
+	Renames renames;	// input
+
+	typedef std::unordered_map<
+		std::string,							/* original name */
+		std::pair<
+			clang::Decl*						/* canonical decl */,
+			std::unordered_set<clang::Decl*>	/* all decls (including canonical) */
+		>
+	> NamedDecls;
+	NamedDecls decls;							// built during first pass
+
+	typedef std::unordered_map<
+		clang::Decl*,								/* canonical decl */
+		std::pair<
+			std::unordered_set<clang::Decl*>,		/* all decls (first pass) */
+			std::unordered_set<clang::DeclRefExpr*> /* all refs (second pass) */
+		>
+	> Refs;
+	Refs refs;
+
+	//clang::SourceManager & sourceManager;
+	//clang::tooling::Replacements & replacements;
+
+	void declsToRefs() {
+		for(auto & d : decls) refs[d.second.first].first = d.second.second;
+	}
 
 
-struct ReplaceRefs : clang::ASTFrontendAction {
-
-	struct Impl : clang::ASTConsumer, clang::RecursiveASTVisitor<Impl> {
-		typedef clang::RecursiveASTVisitor<Impl> Parent;
-
-		clang::SourceManager & sourceManager;
-		clang::StringRef sourceFile;
-		bool indent;
-
-		Impl(clang::SourceManager & sourceManager, clang::StringRef sourceFile):
-			sourceManager(sourceManager), sourceFile(sourceFile), indent(false) {}
-
-		virtual void HandleTranslationUnit(clang::ASTContext & context) override { // from ASTConsumer
-			std::cout << "Parsing file \"" << sourceFile.str() << '"' << std::endl;
-			TraverseDecl(context.getTranslationUnitDecl());
-		}
-
-		bool VisitStmt(clang::Stmt * stmt) { // from RecursiveASTVisitor
-			if(llvm::dyn_cast<clang::DeclRefExpr>(stmt)) {
-				clang::ValueDecl* decl = static_cast<clang::DeclRefExpr*>(stmt)->getDecl();
-
-				if(sourceManager.getFilename(decl->getLocation()).equals(sourceFile)) {
-					std::cout << decl << " " << decl->getQualifiedNameAsString() << std::endl;
-					/*std::cout << 'L' << sourceManager.getExpansionLineNumber(stmt->getLocStart()) << '\t';
-					if(indent) std::cout << '\t';
-					std::cout << "\tUse " << decl->getQualifiedNameAsString() << std::endl;*/
-				}
+	void processDecl(clang::NamedDecl * decl) { // first pass handler
+		auto iter = renames.find(decl->getQualifiedNameAsString());
+		if(iter != renames.end()) {
+			auto & r = decls[iter->first];
+			r.second.insert(decl);
+			if(decl->isCanonicalDecl()) {
+				assert(!r.first);
+				r.first = decl;
 			}
-			return true;
 		}
+	}
 
-		/*bool VisitDecl(clang::Decl *decl) { // from RecursiveASTVisitor
-			bool func = llvm::dyn_cast<clang::FunctionDecl>(decl);
-			if(llvm::dyn_cast<clang::CXXRecordDecl>(decl) || (func && decl->hasBody())) {
-				if(sourceManager.getFilename(decl->getLocation()).equals(sourceFile)) {
-					std::cout << 'L' << sourceManager.getExpansionLineNumber(decl->getLocStart()) << '\t';
-					indent = func && llvm::dyn_cast<clang::CXXMethodDecl>(decl);
-					if(indent) std::cout << '\t';
-					if(func) std::cout << "function ";
-					else std::cout << "class ";
-					std::cout << (static_cast<clang::NamedDecl*>(decl))->getQualifiedNameAsString() << std::endl;
-				}
+	struct ScanDecls : clang::ASTFrontendAction { // first pass traversal
+		ReplaceRefs & parent;
+		ScanDecls(ReplaceRefs & parent) : parent(parent) {}
+
+		struct Impl : clang::ASTConsumer, clang::RecursiveASTVisitor<Impl> {
+			ReplaceRefs & parent;
+			Impl(ReplaceRefs & parent): parent(parent) {}
+
+			void HandleTranslationUnit(clang::ASTContext & context) override { // from ASTConsumer
+				std::cout << "Scanning decls..." << std::endl;
+				TraverseDecl(context.getTranslationUnitDecl());
 			}
-			return true;
-		}*/
+			bool VisitDecl(clang::Decl * decl) { // from RecursiveASTVisitor
+				if(llvm::dyn_cast<clang::NamedDecl>(decl)) parent.processDecl(static_cast<clang::NamedDecl*>(decl));
+				return true;
+			}
+		}; // ScanDecls::Impl
 
-	};
-	virtual Impl* CreateASTConsumer(clang::CompilerInstance& ci, llvm::StringRef sourceFile) override {
-		return new Impl(ci.getSourceManager(), sourceFile);
+		virtual Impl* CreateASTConsumer(clang::CompilerInstance&, llvm::StringRef) override {
+			return new Impl(parent);
+		}
+	}; // ScanDecls
+
+
+
+
+	void processRef(clang::DeclRefExpr * ref) { // second pass handler
+		auto iter = refs.find(ref->getDecl());
+		if(iter != refs.end()) iter->second.second.insert(ref);
+	}
+
+	struct ScanRefs : clang::ASTFrontendAction { // second pass traversal
+		ReplaceRefs & parent;
+		ScanRefs(ReplaceRefs & parent) : parent(parent) {}
+
+		struct Impl : clang::ASTConsumer, clang::RecursiveASTVisitor<Impl> {
+			ReplaceRefs & parent;
+			Impl(ReplaceRefs & parent) : parent(parent) {}
+
+			void HandleTranslationUnit(clang::ASTContext & context) override { // from ASTConsumer
+				std::cout << "Scanning refs..." << std::endl;
+				TraverseDecl(context.getTranslationUnitDecl());
+			}
+
+			bool VisitStmt(clang::Stmt * stmt) { // from RecursiveASTVisitor
+				if(llvm::dyn_cast<clang::DeclRefExpr>(stmt)) parent.processRef(static_cast<clang::DeclRefExpr*>(stmt));
+				return true;
+			}
+		}; // ScanRefs::Impl
+
+		virtual Impl* CreateASTConsumer(clang::CompilerInstance&, llvm::StringRef) override {
+			return new Impl(parent);
+		}
+	}; // ScanRefs
+
+	template<class Action> static clang::tooling::FrontendActionFactory * makeAction(ReplaceRefs & parent) {
+		struct Factory : clang::tooling::FrontendActionFactory {
+			ReplaceRefs & parent;
+			Factory(ReplaceRefs & parent) : parent(parent) {}
+			clang::FrontendAction *create() override { return new Action(parent); }
+		};
+		return new Factory(parent);
 	}
 
 	static int run(const clang::tooling::CompilationDatabase & db, llvm::ArrayRef<std::string> sources) {
 		clang::tooling::ClangTool tool(db, sources);
-		return tool.run(clang::tooling::newFrontendActionFactory<ReplaceRefs>());
+		ReplaceRefs top;
+
+		int r = tool.run(makeAction<ScanDecls>(top));
+		//if(!r) return r;
+		top.declsToRefs();
+		r = tool.run(makeAction<ScanRefs>(top));
+		return r;
+		// TODO : build replacements from refs
 	}
 };
 
